5937b2be58839a001e79a5fc8e7b318f2fa06ab7
anna selyanina <anyaselyanina2000@gmail.com>
2025-12-07 12:44:15 +0500

    дополнен main.py принтами для функций proccesing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import get_date, mask_account_card
 from src.processing import filter_by_state, sort_by_date
+from src.widget import get_date, mask_account_card
 
 print(get_mask_card_number("7000792289606361"))
 print(get_mask_account("73654108430135874305"))
