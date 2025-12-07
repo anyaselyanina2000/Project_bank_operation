15f04196fbf0af65fcdb66ac9214c3bbfba843a9
anna selyanina <anyaselyanina2000@gmail.com>
2025-12-07 12:51:17 +0500

    форматирование кода по flake8

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,7 @@
 from typing import List, Dict, Optional
 
-def filter_by_state(list_dictionary: List[Dict], state: Optional[str]='EXECUTED') -> List[Dict]:
+
+def filter_by_state(list_dictionary: List[Dict], state: Optional[str] = 'EXECUTED') -> List[Dict]:
     """Функция, которая принимает список словарей и опционально значение
      для ключа и возвращает новый список словарей, содержащий только те словари,
       у которых ключ соответствует указанному значению."""
@@ -11,7 +12,7 @@ def filter_by_state(list_dictionary: List[Dict], state: Optional[str]='EXECUTED'
     return list_dictionary_executed
 
 
-def sort_by_date(list_dictionary: List[Dict], parameter: bool=True) -> List[Dict]:
+def sort_by_date(list_dictionary: List[Dict], parameter: bool = True) -> List[Dict]:
     """Функция, которая принимает список словарей и необязательный параметр,
      задающий порядок сортировки и возвращает новый список, отсортированный по дате."""
     sort_list_dictionary = sorted(list_dictionary, key=lambda i: i.get("date"), reverse=parameter)
