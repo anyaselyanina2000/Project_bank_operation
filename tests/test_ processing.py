08937c25cd34b76f0427a36b0da0232e0c6080d1
anna selyanina <anyaselyanina2000@gmail.com>
2026-07-07 20:13:28 +0500

    добавлен тест для processing

---
+++ tests/test_ processing.py
@@ -0,0 +1,40 @@
+import pytest
+
+from src.processing import filter_by_state, sort_by_date
+
+@pytest.fixture
+def operations():
+    return [
+        {'id': '41428829', 'state': 'EXECUTED',
+         'date': '2019-07-03T18:35:29.512364'},
+        {'id': '594226727', 'state': 'CANCELED',
+         'date': '2018-09-12T21:27:25.241689'},
+        {'id': '939719570', 'state': 'EXECUTED',
+         'date': '2018-06-30T02:08:58.425572'}
+    ]
+
+def test_filter_by_state(operations) -> None:
+    """Тестирование фильтрации списка словарей
+    по заданному статусу state."""
+    assert filter_by_state(operations, 'EXECUTED') == [
+        {'id': '41428829', 'state': 'EXECUTED',
+         'date': '2019-07-03T18:35:29.512364'},
+        {'id': '939719570', 'state': 'EXECUTED',
+         'date': '2018-06-30T02:08:58.425572'}
+    ]
+    assert filter_by_state(operations, 'CANCELED') == [
+        {'id': '594226727', 'state': 'CANCELED',
+         'date': '2018-09-12T21:27:25.241689'}
+    ]
+
+def test_sort_by_date(operations) -> None:
+    """Тестирование сортировки списка словарей по
+    датам в порядке убывания и возрастания."""
+    assert sort_by_date(operations) == [
+        {'id': '41428829', 'state': 'EXECUTED',
+         'date': '2019-07-03T18:35:29.512364'},
+        {'id': '594226727', 'state': 'CANCELED',
+         'date': '2018-09-12T21:27:25.241689'},
+        {'id': '939719570', 'state': 'EXECUTED',
+         'date': '2018-06-30T02:08:58.425572'}
+    ]
---
+++ tests/test_masks.py
@@ -0,0 +1,18 @@
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+
+
+@pytest.mark.parametrize('card_number, expected', [
+('7000792289606361', '7000 79** **** 6361')
+])
+def test_get_mask_card_number(card_number, expected):
+    """Тестирование правильности маскирования номера карты."""
+    assert get_mask_card_number(card_number) == expected
+
+@pytest.mark.parametrize("account_number, expected", [
+    ('73654108430135874305', '**4305')
+])
+def test_get_mask_account(account_number, expected):
+    """Тестирование правильности маскирования номера счета."""
+    assert get_mask_account(account_number) == expected
