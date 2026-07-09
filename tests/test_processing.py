197633eac6f479fc41a4b49f9d6b2cbcb3133155
anna selyanina <anyaselyanina2000@gmail.com>
2026-07-09 14:19:04 +0500

    добавлен тест для masks

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,9 +1,12 @@
+from typing import Dict, List
+
 import pytest
 
 from src.processing import filter_by_state, sort_by_date
 
+
 @pytest.fixture
-def operations():
+def operations() -> List[Dict[str, str]]:
     return [
         {'id': '41428829', 'state': 'EXECUTED',
          'date': '2019-07-03T18:35:29.512364'},
@@ -13,7 +16,8 @@ def operations():
          'date': '2018-06-30T02:08:58.425572'}
     ]
 
-def test_filter_by_state(operations) -> None:
+
+def test_filter_by_state(operations: List[Dict[str, str]]) -> None:
     """Тестирование фильтрации списка словарей
     по заданному статусу state."""
     assert filter_by_state(operations, 'EXECUTED') == [
@@ -27,7 +31,8 @@ def test_filter_by_state(operations) -> None:
          'date': '2018-09-12T21:27:25.241689'}
     ]
 
-def test_sort_by_date(operations) -> None:
+
+def test_sort_by_date(operations: List[Dict[str, str]]) -> None:
     """Тестирование сортировки списка словарей по
     датам в порядке убывания и возрастания."""
     assert sort_by_date(operations) == [
